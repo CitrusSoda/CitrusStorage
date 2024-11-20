afbb0cd84c8bf739f93ac34baa60e5b2b1a5c53c
CitrusSoda <dndhkaus1004@naver.com>
2024-11-20 17:43:55 +0900

    feat: auth 폼 기반 구현

---
+++ components/AuthForm.tsx
@@ -0,0 +1,134 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === 'sign-up'
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
+type FormType = 'sign-in' | 'sign-up';
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: '',
+      email: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="h1">{type === 'sign-in' ? '로그인' : '회원가입'}</h1>
+          {type === 'sign-up' && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">이름</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="이름을 입력해주세요"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">이메일</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="이메일을 입력해주세요"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === 'sign-in' ? '로그인' : '회원가입'}
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === 'sign-in'
+                ? '아직 계정이 없으신가요?'
+                : '이미 계정이 있으신가요?'}
+            </p>
+            <Link
+              href={type === 'sign-in' ? '/sign-up' : '/sign-in'}
+              className="ml-1 font-medium text-brand"
+            >
+              {type === 'sign-in' ? '회원가입' : '로그인'}
+            </Link>
+          </div>
+        </form>
+        {/* OTP */}
+      </Form>
+    </>
+  );
+};
+
+export default AuthForm;
